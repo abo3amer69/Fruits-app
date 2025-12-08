f03f2e7efe1be06f319328a0134d1cbc99229bae
Ahmed Amer <AhmedAmer>
2025-12-08 13:54:59 +0200

    create splash view ui

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_apps/core/utils/app_images.dart';
+import 'package:svg_flutter/svg_flutter.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [SvgPicture.asset(Assets.imagesPlant)],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(Assets.imagesSplashBottom,
+        fit: BoxFit.fill,
+        ),
+      ],
+    );
   }
 }
