6e9dc21a5dc3d599fd48d80756f87b62e914ff49
Ahmed Amer <AhmedAmer>
2025-12-09 16:51:21 +0200

    part 4 "custo text form field"

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_apps/constants.dart';
+import 'package:fruits_apps/core/utils/app_colors.dart';
+import 'package:fruits_apps/core/utils/app_text_styles.dart';
+import 'package:fruits_apps/core/widgets/custom_button.dart';
 import 'package:fruits_apps/core/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -22,6 +25,46 @@ class LoginViewBody extends StatelessWidget {
               suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
               hintText: 'كلمة المرور ',
               textInputType: TextInputType.visiblePassword,
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'نسيت كلمة المرور؟',
+                  style: TextStyles.semiBold13.copyWith(
+                    color: AppColors.lightPrimaryColor,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 33),
+            CustomButton(onPressed: () {}, text: 'تسجيل الدخول'),
+             SizedBox(height: 33),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'ليس لديك حساب؟ ',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '  ',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: Color(0xff616A6B),
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'انشاء حساب',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: Color(0xff949D9E),
+                    ),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
