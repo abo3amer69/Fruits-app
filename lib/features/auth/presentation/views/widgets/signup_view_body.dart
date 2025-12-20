69d2dc9671892df8808337a091f306d012b582fb
Ahmed Amer <AhmedAmer>
2025-12-20 17:36:17 +0200

    signup ui part 2

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_apps/constants.dart';
+import 'package:fruits_apps/core/widgets/custom_text_form_field.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+        child: Column(
+           children: [
+            SizedBox(height: 24,),
+             CustomTextFormField(
+                hintText: 'الاسم كامل',
+                textInputType: TextInputType.name,
+              ),
+              SizedBox(height: 16),
+              CustomTextFormField(
+               
+                hintText: 'البريد الالكتروني',
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 16),
+
+               CustomTextFormField(
+               suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
+                hintText: 'البريد الالكتروني',
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 16),
+           ],
+        ),
+      ),
+    );
   }
 }
