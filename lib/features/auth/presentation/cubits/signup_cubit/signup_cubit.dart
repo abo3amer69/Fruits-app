08b477facaf74c0cd144c3e86708ea3025fc02bf
Ahmed Amer <AhmedAmer>
2025-12-21 14:51:34 +0200

    create signup cubit

---
+++ lib/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruits_apps/features/auth/domain/entities/user_entity.dart';
+import 'package:fruits_apps/features/auth/domain/repos/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'signup_state.dart';
+
+class SignupCubit extends Cubit<SignupState> {
+  SignupCubit(this.authRepo) : super(SignupInitial());
+
+  final AuthRepo authRepo;
+  Future<void> creatUserWithEmailAndPassword(
+    String email,
+    String password,
+    String name,
+  ) async {
+    emit(SignupLoading());
+    final result = await authRepo.creatUserWithEmaimAndPassword(
+      email,
+      password,
+      name,
+    );
+    result.fold(
+      (failure) => emit(SignupFailure(message: failure.message)),
+      (user) => emit(SignupSuccess(userEntity: user)),
+    );
+  }
+}
