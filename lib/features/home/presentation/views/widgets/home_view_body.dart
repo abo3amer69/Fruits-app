b20b9fb6e999df61c2b97ccc1f5628d3aeb9a1ab
Ahmed Amer <AhmedAmer>
2025-12-30 22:18:05 +0200

    create futured item

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fruits_apps/constants.dart';
 import 'package:fruits_apps/core/widgets/search_text_field.dart';
 import 'package:fruits_apps/features/home/presentation/views/widgets/custom_home_app_bar.dart';
+import 'package:fruits_apps/features/home/presentation/views/widgets/featured_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -19,6 +20,8 @@ class HomeViewBody extends StatelessWidget {
                 CustomHomeAppBar(),
                 SizedBox(height: 16),
                 SearchTextField(),
+                SizedBox(height: 12,),
+                FeaturedItem(),
               ],
             ),
           ),
