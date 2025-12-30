65fa917f56c2963f954c85f15f61cb11209a2767
Ahmed Amer <AhmedAmer>
2025-12-30 16:03:08 +0200

    implement add user data

--- lib/core/services/get_it_services.dart
+++ lib/core/services/get_it_services.dart
@@ -1,4 +1,6 @@
+import 'package:fruits_apps/core/services/database_services.dart';
 import 'package:fruits_apps/core/services/firebase_auth_services.dart';
+import 'package:fruits_apps/core/services/firestore_services.dart';
 import 'package:fruits_apps/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:fruits_apps/features/auth/domain/repos/auth_repo.dart';
 import 'package:get_it/get_it.dart';
@@ -7,7 +9,10 @@ final getIt = GetIt.instance;
 
 void setUpGetIt() {
   getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+  getIt.registerSingleton<DatabaseServices>(FirestoreServices());
   getIt.registerSingleton<AuthRepo>(
-    AuthRepoImpl(firebaseAuthServices: getIt<FirebaseAuthServices>()),
+    AuthRepoImpl(firebaseAuthServices: getIt<FirebaseAuthServices>(),
+        databaseServices: getIt<DatabaseServices>(),
+    )
   );
 }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -3,15 +3,21 @@ import 'dart:developer';
 import 'package:dartz/dartz.dart';
 import 'package:fruits_apps/core/errors/exceptions.dart';
 import 'package:fruits_apps/core/errors/failures.dart';
+import 'package:fruits_apps/core/services/database_services.dart';
 import 'package:fruits_apps/core/services/firebase_auth_services.dart';
+import 'package:fruits_apps/core/utils/backend_endpoint.dart';
 import 'package:fruits_apps/features/auth/data/models/user_model.dart';
 import 'package:fruits_apps/features/auth/domain/entities/user_entity.dart';
 import 'package:fruits_apps/features/auth/domain/repos/auth_repo.dart';
 
 class AuthRepoImpl extends AuthRepo {
   final FirebaseAuthServices firebaseAuthServices;
+  final DatabaseServices databaseServices;
 
-  AuthRepoImpl({required this.firebaseAuthServices});
+  AuthRepoImpl({
+    required this.firebaseAuthServices,
+    required this.databaseServices,
+  });
   @override
   Future<Either<Failure, UserEntity>> creatUserWithEmaimAndPassword(
     String email,
@@ -23,7 +29,9 @@ class AuthRepoImpl extends AuthRepo {
         email: email,
         password: password,
       );
-      return Right(UserModel.fromFirebaseUser(user));
+      var userEntity = UserModel.fromFirebaseUser(user);
+      await addData(user: userEntity);
+      return Right(userEntity);
     } on CustomException catch (e) {
       return left(ServerFailure(e.message));
     } catch (e) {
@@ -75,5 +83,13 @@ class AuthRepoImpl extends AuthRepo {
       log('Exception in AuthRepoImpl signInWithFacebook: ${e.toString()}');
       return left(ServerFailure('لقد حدث خطأ ما، الرجاء المحاولة لاحقاً.'));
     }
+  }
+
+  @override
+  Future<dynamic> addData({required UserEntity user}) async {
+    await databaseServices.addData(
+      path: BackendEndpoint.addUserData,
+      data: user.toMap(),
+    );
   }
 }
