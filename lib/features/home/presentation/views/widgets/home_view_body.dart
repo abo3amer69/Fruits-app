a3b6d666df4c69b303c4e78f3c81665a8a22af64
Ahmed Amer <AhmedAmer>
2025-12-30 21:56:10 +0200

    create search text field

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_apps/constants.dart';
+import 'package:fruits_apps/core/widgets/search_text_field.dart';
 import 'package:fruits_apps/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -6,12 +8,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: CustomHomeAppBar(),
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                SizedBox(height: kTopPaddding),
+                CustomHomeAppBar(),
+                SizedBox(height: 16),
+                SearchTextField(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
