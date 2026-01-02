1aa00eced05ddf2bc16d6073fda13c1220a2ce9a
Ahmed Amer <AhmedAmer>
2026-01-02 18:06:49 +0200

    featured item done

--- lib/features/home/presentation/views/widgets/featured_item.dart
+++ lib/features/home/presentation/views/widgets/featured_item.dart
@@ -1,34 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart'as svg;
 import 'package:fruits_apps/core/utils/app_images.dart';
 import 'package:fruits_apps/core/utils/app_text_styles.dart';
 import 'package:fruits_apps/features/home/presentation/views/widgets/featured_item_button.dart';
+import 'package:svg_flutter/svg.dart';
 
 class FeaturedItem extends StatelessWidget {
   const FeaturedItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var itemWidth = MediaQuery.sizeOf(context).width;
     return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
-      child: Stack(
-        children: [
-          Image.asset(Assets.imagesWatermelonTest),
-          Container(
-            child: Column(
-              children: [
-                Text(
-                  'عروض العيد',
-                  style: TextStyles.regular13.copyWith(color: Colors.white),
-                ),
-                Text(
-                  'خصم 50%  ',
-                  style: TextStyles.bold19.copyWith(color: Colors.white),
-                ),
-                FeaturedItemButton(onPressed: (){}),
-              ],
+      width: itemWidth,
+      child: AspectRatio(
+        aspectRatio: 348 / 158,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              top: 0,
+              bottom: 0,
+              right: itemWidth * .4,
+              child: SvgPicture.asset(Assets.imagesPageViewItem2Image, fit: BoxFit.fill),
             ),
-          ),
-        ],
+            Container(
+              width: itemWidth * .5,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: svg.Svg(Assets.imagesFeaturedItemBackground),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(right: 33),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 25),
+                    Text(
+                      'عروض العيد',
+                      style: TextStyles.regular13.copyWith(color: Colors.white),
+                    ),
+                    Spacer(),
+                    Text(
+                      'خصم 50%  ',
+                      style: TextStyles.bold19.copyWith(color: Colors.white),
+                    ),
+                    SizedBox(height: 11),
+                    FeaturedItemButton(onPressed: () {}),
+                    SizedBox(height: 29),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
